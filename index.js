1709ddc84fe150fdf8e28f84ba277065c7e46c4d
iahmedfarag <ahmedfarag1k@gmail.com>
2024-03-20 00:52:47 +0200

    update

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ app.get("/", (req, res) => {
 app.use("/products", productsRouter);
 app.use("/categories", categoriesRouter);
 
-app.get("*", res.json({ status: "not found", message: "no page found" }));
-
 app.listen(3000 || process.env.PORT, () => {
     console.log("server is listening on port 3000");
 });
